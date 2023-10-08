0ac921b0f5a40640ef786ccffb784f7338a9146d
mikhailir <saburov1985@gmail.com>
2023-10-08 21:22:20 +1100

    Верстка heder и добавил пакет macro-css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 function App() {
   return (
-    <div className="App">
-      <h1>SneakersShop</h1>
+    <div className="wrapper">
+      <header className="d-flex">
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" alt="Логотип" />
+          <div className="headerInfo">
+            <h3>React Sneackers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+
+        <ul className="headerRight">
+          <li>
+            <img src="/img/cart.svg" alt="Корзина" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img src="/img/user.svg" alt="Пользователь" />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+        ...
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import 'macro-css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
