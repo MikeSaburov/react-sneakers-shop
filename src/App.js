84f1056cf3180c8fcde3e503e1aee78b092bc7d3
mikhailir <saburov1985@gmail.com>
2023-10-09 22:20:40 +1100

    Создал компонент Drawer(Корзина)

--- src/App.js
+++ src/App.js
@@ -1,78 +1,11 @@
 import { Card } from './components/Card';
 import { Header } from './components/Header';
+import { Drawer } from './components/Drawer';
 
 function App() {
   return (
     <div className="wrapper clear">
-      <aside style={{ display: 'none' }} className="overlay">
-        <div className="drawer">
-          <h2 className=" d-flex justify-between mb-30 ">
-            Корзина{' '}
-            <img
-              className="removeBtn cu-p"
-              width={32}
-              height={32}
-              src="/img/remove.svg"
-              alt="Удалить"
-            />
-          </h2>
-
-          <div className="items">
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-                className="cartItemImg"
-              ></div>
-              <div className="mr-20 ">
-                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 990 руб.</b>
-              </div>
-              <img
-                className="removeBtn"
-                width={32}
-                height={32}
-                src="/img/remove.svg"
-                alt="Удалить"
-              />
-            </div>
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{ backgroundImage: 'url(/img/sneakers/3.jpg)' }}
-                className="cartItemImg"
-              ></div>
-              <div className="mr-20 ">
-                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 990 руб.</b>
-              </div>
-              <img
-                className="removeBtn"
-                width={32}
-                height={32}
-                src="/img/remove.svg"
-                alt="Удалить"
-              />
-            </div>
-          </div>
-          <div className="cartTotalBlock">
-            <ul>
-              <li>
-                <span>Итого:</span>
-                <div></div>
-                <b>21 498 руб. </b>
-              </li>
-              <li>
-                <span>Налог 5%</span>
-                <div></div>
-                <b>1074 руб. </b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              Оформить заказ{' '}
-              <img width={14} height={12} src="/img/arrow.svg" alt="Стрелка" />
-            </button>
-          </div>
-        </div>
-      </aside>
+      <Drawer />
       <Header />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
