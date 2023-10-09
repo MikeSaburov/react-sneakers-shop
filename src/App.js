a01a831b782e6a15a30524b241319eaa95b1774c
mikhailir <saburov1985@gmail.com>
2023-10-09 18:47:52 +1100

    Сверстал корзину

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ function App() {
     <div className="wrapper clear">
       <aside className="overlay">
         <div className="drawer">
-          <h2 className="mb-20">Корзина</h2>
+          <h2 className=" d-flex justify-between mb-30 ">
+            Корзина{' '}
+            <img
+              className="removeBtn cu-p"
+              width={32}
+              height={32}
+              src="/img/remove.svg"
+              alt="Удалить"
+            />
+          </h2>
+
           <div className="items">
             <div className="cartItem d-flex align-center mb-20">
               <div
@@ -19,12 +29,12 @@ function App() {
                 width={32}
                 height={32}
                 src="/img/remove.svg"
-                alt="Удалить из корзины"
+                alt="Удалить"
               />
             </div>
             <div className="cartItem d-flex align-center mb-20">
               <div
-                style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+                style={{ backgroundImage: 'url(/img/sneakers/3.jpg)' }}
                 className="cartItemImg"
               ></div>
               <div className="mr-20 ">
@@ -36,9 +46,27 @@ function App() {
                 width={32}
                 height={32}
                 src="/img/remove.svg"
-                alt="Удалить из корзины"
+                alt="Удалить"
               />
             </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ{' '}
+              <img width={14} height={12} src="/img/arrow.svg" alt="Стрелка" />
+            </button>
           </div>
         </div>
       </aside>
