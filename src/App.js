13d9a6d7d697985a0b6a1e7a9231dbb359785cb0
mikhailir <saburov1985@gmail.com>
2023-10-11 21:30:07 +1100

    Реализовал поиск по названию кроссовок

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Drawer } from './components/Drawer';
 function App() {
   const [items, setItems] = useState([]);
   const [cartItems, setcartItems] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const [cartOpened, setCartOpened] = useState(false);
 
   useEffect(() => {
@@ -22,6 +23,11 @@ function App() {
     setcartItems((prev) => [...prev, obj]);
   };
 
+  const onChangeSearchInput = (event) => {
+    //console.log();
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="wrapper clear">
       {cartOpened && (
@@ -30,28 +36,50 @@ function App() {
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue
+              ? `Поиск по запросу: ${searchValue}`
+              : ' Все кроссовки'}
+          </h1>
           <div className="searchBlock d-flex align-center">
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue('')}
+                className="clear"
+                src="/img/remove.svg"
+                alt="Очистить"
+              />
+            )}
+
             <img
               width={14.25}
               height={14.25}
               src="/img/search.svg"
               alt="Поиск"
             />
-            <input type="text" placeholder="Поиск..." />
+            <input
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              type="text"
+              placeholder="Поиск..."
+            />
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Card
-              key={item.imgUrl} //Здесь взял за key ссылку на изображение, т.к. это единственное уникальное значение
-              title={item.title}
-              price={item.price}
-              imgUrl={item.imgUrl}
-              onFavorite={() => console.log('Добавили в закладки')}
-              onAddCart={(obj) => onAddToCart(obj)}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item) => (
+              <Card
+                key={item.imgUrl} //Здесь взял за key ссылку на изображение, т.к. это единственное уникальное значение
+                title={item.title}
+                price={item.price}
+                imgUrl={item.imgUrl}
+                onFavorite={() => console.log('Добавили в закладки')}
+                onAddCart={(obj) => onAddToCart(obj)}
+              />
+            ))}
         </div>
       </div>
     </div>
